703e5dee400266ff463b74e83c2811ba13c82a2b
Sławek Rzeppa <srzeppa@sigma.ug.edu.pl>
2016-04-29 21:14:31 +0200

    simple menu added

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,22 @@ namespace movies
         public MainPage()
         {
             this.InitializeComponent();
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size { Width = 320, Height = 320 });
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SearchListBoxItem.IsSelected) { ResultTextBlock.Text = "Share"; }
+            else if (FavoritesListBoxItem.IsSelected) { ResultTextBlock.Text = "Favorites"; }
+            else if (ViewedListBoxItem.IsSelected) { ResultTextBlock.Text = "Viewed"; }
+
         }
     }
 }
+
+//https://github.com/Windows-Readiness/AbsoluteBeginnersWin10/blob/master/UWP-021/UWP-021/HamburgerExample/HamburgerExample/MainPage.xaml.cs
