a829dfd0d5283cd612de40de62ad338aed8f9a79
Sławek Rzeppa <srzeppa@sigma.ug.edu.pl>
2016-04-29 21:13:07 +0200

    simple menu added

---
+++ Model/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movies.Model
+{
+    class Movie
+    {
+
+        private String title { get; set; }
+        private int year { get; set; }
+        private String rated { get; set; }
+        private DateTime relased { get; set; }
+        private String runtime { get; set; }
+        private String genre { get; set; }
+        private String director { get; set; }
+        private String writer { get; set; }
+        private String actors { get; set; }
+        private String plot { get; set; }
+        private String language { get; set; }
+        private String country { get; set; }
+        private String awards { get; set; }
+        private String poster { get; set; }
+        private int metascore { get; set; }
+        private double imdbRating { get; set; }
+        private double imdbVotes { get; set; }
+        private String imdbId { get; set; }
+        private String type { get; set; }
+        private Boolean response { get; set; }
+
+    }
+}
