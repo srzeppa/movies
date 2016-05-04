f971829bc8215b47c98f4cad3595d85d6eac3265
Sławek Rzeppa <srzeppa@sigma.ug.edu.pl>
2016-05-04 19:02:44 +0200

    Creating sqlite db works

--- ViewModel/CreateDatabase.cs
+++ ViewModel/CreateDatabase.cs
@@ -12,11 +12,15 @@ namespace movies.ViewModel
 
         public void createDB()
         {
-            string path;
-            SQLite.Net.SQLiteConnection conn;
-            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "db.movies");
-            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
-            conn.CreateTable<Model.UserMovie>();
+            //AppData\Local\Packages\6ed022d9-e2e8-4be6-92e6-4d110cb7293e_gadjc3dd055ja
+            if (!File.Exists(Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "movies.sqlite")))
+            {
+                string path;
+                SQLite.Net.SQLiteConnection conn;
+                path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "movies.sqlite");
+                conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+                conn.CreateTable<Model.UserMovie>();
+            }
         }
 
     }
