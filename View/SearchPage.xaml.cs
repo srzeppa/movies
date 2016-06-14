8d045f94f3ef18e80ff7d04dfd764123258a5045
Sławek Rzeppa <srzeppa@sigma.ug.edu.pl>
2016-06-14 19:16:19 +0200

    MVVM WORKS WELLLLLLLLLLLLLLL BUAHUAHUAHAUHAUHAUAHUAHAUHAUHU ENJOY

--- View/SearchPage.xaml.cs
+++ View/SearchPage.xaml.cs
@@ -23,49 +23,10 @@ namespace movies.View
 {
     public sealed partial class SearchPage : Page
     {
-        Movie movie;
         public SearchPage()
         {
-            InitializeComponent();
-        }
-
-        private async void buttonSearch_Click(object sender, RoutedEventArgs e)
-        {
-            String title = textBoxSearch.Text;
-            if (title != null || title != "")
-            {
-                movie = await JsonParser.getMovieByTitle(title);
-                if (movie.title != null)
-                {
-                    if (movie.poster != null)
-                    {
-                        posterImage.Source = new BitmapImage(new Uri(movie.poster));
-                    }
-                    movieTextBlock.Text = "Title: " + movie.title + "\r\nYear: " + movie.year + "\r\nDirector: " + movie.director + "Writer: " + movie.writer + "\r\nActors: " + movie.actors + "\r\nGenre: " + movie.genre + "\r\nCountry: " + movie.country + "\r\nLanguage: " + movie.language + "\r\nAwards: " + movie.awards;
-                    posterTextBlock.Text = movie.plot;
-                    Rating.Value = movie.imdbRating;
-                    ToWatchButton.Visibility = Visibility.Visible;
-                } else
-                {
-                    movieTextBlock.Text = "this movie is not in our database";
-                }
-            }
-
-        }
-
-        private void ToWatchButton_Click(object sender, RoutedEventArgs e)
-        {
-            userToWatchDatePicker.Visibility = Visibility.Visible;
-        }
-
-        private void userToWatchDatePicker_DateChanged(object sender, DatePickerValueChangedEventArgs e)
-        {
-            DateTime date = userToWatchDatePicker.Date.DateTime;
-            ViewModel.DatabaseHandler insertToDatabase = new ViewModel.DatabaseHandler();
-            if (date > DateTime.Now)
-            {
-                insertToDatabase.insert(movie.imdbId, 0, date);
-            }
+            this.InitializeComponent();
+            DataContext = new SearchMovieViewModel();
         }
     }
 }
---
+++ ViewModel/SearchMovieViewModel.cs
@@ -0,0 +1,105 @@
+﻿using movies.Command;
+using movies.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace movies.ViewModel
+{
+    class SearchMovieViewModel : MainViewModel
+    {
+        private Movie movie_ = null;
+        private String titleToSearch;
+        private String content;
+        private String plot;
+        private String poster;
+
+        public String Title
+        {
+            get
+            {
+                return titleToSearch;
+            }
+            set
+            {
+                titleToSearch = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        public String Content
+        {
+            get
+            {
+                return content;
+            }
+            set
+            {
+                content = value;
+                OnPropertyChanged("Content");
+            }
+        }
+
+        public String DisplayedPosterPath
+        {
+            get
+            {
+                return poster;
+            }
+            set
+            {
+                poster = value;
+                OnPropertyChanged("DisplayedPosterPath");
+            }
+        }
+
+        public String Plot
+        {
+            get
+            {
+                return plot;
+            }
+            set
+            {
+                plot = value;
+                OnPropertyChanged("Plot");
+            }
+        }
+
+        public ICommand GetMovieCommand
+        {
+            get
+            {
+                return new GetMovieCommand(getMovieByTitle);
+            }
+        }
+
+        public async void getMovieByTitle()
+        {
+            var http = new HttpClient();
+            string url = String.Format("http://www.omdbapi.com/?t=" + Title + "&y=&plot=short&r=json");
+            var response = await http.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+            Movie movie = JsonConvert.DeserializeObject<Movie>(result);
+
+            BitmapImage src = new BitmapImage();
+            src.UriSource = new Uri(movie.poster);
+
+            Title = movie.title;
+            Plot = movie.plot;
+            DisplayedPosterPath = movie.poster;
+
+            Content = "Title: " + movie.title + "\r\nYear: " + movie.year + "\r\nDirector: " + movie.director + "Writer: " + movie.writer + "\r\nActors: " + movie.actors + "\r\nGenre: " + movie.genre + "\r\nCountry: " + movie.country + "\r\nLanguage: " + movie.language + "\r\nAwards: " + movie.awards;
+        }
+
+    }
+}
